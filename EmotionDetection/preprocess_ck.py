a2a16eb5be862245c7e0fd5947a5dd483ec03526
7138gmhampton <0407435@abertay.ac.uk>
2020-07-15 21:12:46 +0100

    Add prepared scripts

---
+++ EmotionDetection/preprocess_ck.py
@@ -0,0 +1,143 @@
+import os, numpy, random, hyper
+
+from keras.preprocessing.image import load_img, img_to_array
+
+import matplotlib.pyplot as pyplot
+
+#test_img = load_img('example.png', color_mode='grayscale')
+
+#print(type(test_img))
+
+#img_array = img_to_array(test_img)
+
+#print(img_array)
+#print(img_array.shape)
+
+#pyplot.figure()
+#pyplot.imshow(img_array.reshape((490,640)), interpolation='none', cmap='gray')
+#pyplot.show()
+
+#WIDTH, HEIGHT = 640, 490
+#SCALE_FACTOR = 2
+#height_resize = HEIGHT/SCALE_FACTOR
+#image_size = (int(HEIGHT/SCALE_FACTOR), int(WIDTH/SCALE_FACTOR))
+image_size = (hyper.ROWS, hyper.COLS)
+
+class ImageForCNN:
+    def __init__(self, image_array, emotion):
+        self.image_array = image_array
+        self.emotion = emotion
+
+def prepare_image_for_cnn(file, emotion_code):
+    image = load_img(file, color_mode='grayscale', target_size=image_size)
+
+    image_array = img_to_array(image)
+
+    #return (image_array, emotion_code)
+    return ImageForCNN(image_array, emotion_code)
+
+def load_entire_emotion(directory, emotion_code):
+    images_of_emotion = []
+
+    for file in os.listdir(os.fsencode(directory)):
+        filename = directory + '\\' + os.fsdecode(file)
+        images_of_emotion.append(prepare_image_for_cnn(filename, emotion_code))
+
+    return images_of_emotion
+
+#images = []
+#for file in os.listdir(os.fsencode("000 neutral")):
+#    filename = "000 neutral\\" + os.fsdecode(file)
+#    images.append(prepare_image_for_cnn(filename,0))
+
+#for iii in range(5):
+#    pyplot.figure(iii)
+#    pyplot.imshow(images[iii].image_array.reshape((490,640)), interpolation='none', cmap='gray')
+#pyplot.show()
+
+#print(images[0].image_array.shape)
+
+#pyplot.figure()
+#pyplot.imshow(images[0].image_array.reshape((490,640)), interpolation='none', cmap='gray')
+#pyplot.show()
+
+directories = [('000 neutral',0), 
+               ('001 surprise',1),
+               ('002 sadness',2),
+               ('003 fear',3),
+               ('004 anger',4),
+               ('005 disgust',5),
+               ('006 joy',6)]
+
+#directories = [('000 neutral',0)]
+
+prepared_images = []
+
+for directory in directories:
+    prepared_images.extend(load_entire_emotion(directory[0],directory[1]))
+
+random.shuffle(prepared_images)
+
+#pyplot.figure()
+#pyplot.imshow(prepared_images[-1].image_array.reshape((490,640)), interpolation='none', cmap='gray')
+#pyplot.show()
+
+# Extract Trainable Data and Labels
+dataset = []
+#trainable_data = numpy.array()
+indexed_labels = []
+for prepared_image in prepared_images:
+    #dataset.append(prepared_image.image_array)
+    #image_array = prepared_image.image_array.reshape(490,640)
+    #image_array = numpy.delete(image_array,2,1)
+    image_array = numpy.asarray(prepared_image.image_array).squeeze()
+    #image_array = image_array.astype('float32')
+    #image_array = numpy.expand_dims(prepared_image.image_array, 0)
+    #image_array = numpy.expand_dims(image_array, 0)
+    dataset.append(image_array.astype('float32'))
+    indexed_labels.append(prepared_image.emotion)
+
+#pyplot.figure()
+#pyplot.imshow(dataset[42].reshape((490,640)), interpolation='none', cmap='gray')
+#pyplot.show()
+
+# Output Trainable Data as Array
+print(len(dataset))
+print(dataset[0].shape)
+print(dataset[0].dtype)
+#print(dataset[0])
+#dataset = numpy.asarray(dataset)
+#dataset = numpy.stack(dataset)
+#dataset = numpy.concatenate(dataset, axis=0)
+#dataset = numpy.array(dataset)
+#print(dataset.shape)
+#print(dataset.dtype)
+#print(dataset)
+#trainable_data = numpy.array()
+#dataset = numpy.asarray(dataset)
+#trainable_data = numpy.stack(dataset)
+#trainable_data = numpy.expand_dims(dataset[0],0)
+
+#for image_array in dataset:
+#    appending_array = numpy.expand_dims(image_array,0)
+#    trainable_data = numpy.append(trainable_data, appending_array,0)
+trainable_data = numpy.asarray(dataset)
+trainable_data = numpy.expand_dims(trainable_data, -1)
+print(trainable_data.shape)
+print(trainable_data.dtype)
+#print(trainable_data)
+numpy.save('ck_data', trainable_data)
+print(' -- Trainable data saved --')
+
+# Output Labels
+indexed_labels = numpy.asarray(indexed_labels)
+numpy.save('ck_labels', indexed_labels)
+print(' -- Labels saved -- ')
+
+# Check Output
+#pyplot.figure()
+#pyplot.imshow(trainable_data[0], interpolation='none', cmap='gray')
+for iii in range(5):
+    pyplot.figure(iii).suptitle(indexed_labels[iii])
+    pyplot.imshow(trainable_data[iii].reshape(image_size), interpolation='none', cmap='gray')
+pyplot.show()
---
+++ EmotionDetection/train_ck.py
@@ -0,0 +1,106 @@
+import numpy, hyper
+import matplotlib.pyplot as pyplot
+
+from sklearn.model_selection import train_test_split
+
+# Hyperparameters
+no_of_features = hyper.NO_OF_FEATURES
+no_of_labels = hyper.NO_OF_LABELS
+batch_size = hyper.BATCH_SIZE
+no_of_epochs = hyper.NO_OF_EPOCHS
+rows, cols = hyper.ROWS, hyper.COLS
+
+# Load Training Data
+data = numpy.load('ck_data.npy')
+labels = numpy.load('ck_labels.npy')
+
+# Check Load from File
+#for iii in range(3):
+#    pyplot.figure(iii).suptitle(labels[iii])
+#    pyplot.imshow(data[iii].reshape((HEIGHT,WIDTH)), interpolation='none', cmap='gray')
+#pyplot.show()
+
+# Standardise
+data -= numpy.mean(data, 0)
+data /= numpy.std(data, 0)
+
+# Check Standardise
+#for iii in range(3):
+#    pyplot.figure(iii).suptitle(labels[iii])
+#    pyplot.imshow(data[iii].reshape((HEIGHT,WIDTH)), interpolation='none', cmap='gray')
+#pyplot.show()
+
+# Change Labels to Categorical
+from keras.utils import to_categorical
+labels = to_categorical(labels)
+
+# Section Data
+data_train, data_test, labels_train, labels_test = train_test_split(data,labels, test_size=0.1, random_state=42)
+data_train, data_valid, labels_train, labels_valid = train_test_split(data_train,labels_train, test_size=0.1, random_state=42)
+
+# Save Test Data
+numpy.save('ck_test_data', data_test)
+numpy.save('ck_test_labels', labels_test)
+print(' -- Test Data Saved --')
+
+# CNN Pipeline
+from keras.models import Sequential
+from keras.layers import Dense, Dropout, Activation, Flatten, Conv2D, MaxPooling2D, BatchNormalization
+from keras.losses import categorical_crossentropy
+from keras.optimizers import Adam
+from keras.regularizers import l2
+
+model = Sequential()
+
+model.add(Conv2D(no_of_features, kernel_size=(3, 3), activation='relu', 
+    input_shape=(rows, cols, 1), data_format='channels_last', kernel_regularizer=l2(0.01)))
+model.add(Conv2D(no_of_features, kernel_size=(3, 3), activation='relu', padding='same'))
+model.add(BatchNormalization())
+model.add(MaxPooling2D(pool_size=(2, 2), strides=(2, 2)))
+model.add(Dropout(0.5))
+
+model.add(Conv2D(2*no_of_features, kernel_size=(3, 3), activation='relu', padding='same'))
+model.add(BatchNormalization())
+model.add(Conv2D(2*no_of_features, kernel_size=(3, 3), activation='relu', padding='same'))
+model.add(BatchNormalization())
+model.add(MaxPooling2D(pool_size=(2, 2), strides=(2, 2)))
+model.add(Dropout(0.5))
+
+model.add(Conv2D(2*2*no_of_features, kernel_size=(3, 3), activation='relu', padding='same'))
+model.add(BatchNormalization())
+model.add(Conv2D(2*2*no_of_features, kernel_size=(3, 3), activation='relu', padding='same'))
+model.add(BatchNormalization())
+model.add(MaxPooling2D(pool_size=(2, 2), strides=(2, 2)))
+model.add(Dropout(0.5))
+
+model.add(Conv2D(2*2*2*no_of_features, kernel_size=(3, 3), activation='relu', padding='same'))
+model.add(BatchNormalization())
+model.add(Conv2D(2*2*2*no_of_features, kernel_size=(3, 3), activation='relu', padding='same'))
+model.add(BatchNormalization())
+model.add(MaxPooling2D(pool_size=(2, 2), strides=(2, 2)))
+model.add(Dropout(0.5))
+
+model.add(Flatten())
+
+model.add(Dense(2*2*2*no_of_features, activation='relu'))
+model.add(Dropout(0.5))
+model.add(Dense(2*2*no_of_features, activation='relu'))
+model.add(Dropout(0.5))
+model.add(Dense(2*no_of_features, activation='relu'))
+model.add(Dropout(0.5))
+
+model.add(Dense(no_of_labels, activation='softmax'))
+
+model.summary()
+
+# Compile Model
+model.compile(loss=categorical_crossentropy, optimizer=Adam(), metrics=['accuracy'])
+
+# Train Model
+model.fit(numpy.array(data_train), 
+          numpy.array(labels_train), 
+          batch_size=batch_size, 
+          epochs=no_of_epochs,
+          verbose=1,
+          validation_data=(numpy.array(data_valid), numpy.array(labels_valid)),
+          shuffle=True)
