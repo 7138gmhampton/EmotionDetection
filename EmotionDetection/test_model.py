55103c6d1bb2ebfb24a243478720a03353772ccb
7138gmhampton <0407435@abertay.ac.uk>
2020-07-16 14:53:42 +0100

    Add function to load the model and weights from a file

--- EmotionDetection/test_model.py
+++ EmotionDetection/test_model.py
@@ -1,7 +1,28 @@
-import argparse
+import argparse, os, hyper
 
+from keras.models import model_from_json, Sequential
+
+def acquire_model(timestamp):
+    directory = hyper.MODEL_DIRECTORY
+    model_name = timestamp + '_model.json'
+    weights_name = timestamp + '_weights.h5'
+    
+    with open(os.path.join(directory, model_name), 'r') as json_model:
+        #return model_from_json(json_model.read())
+        model = model_from_json(json_model.read())
+    model.load_weights(os.path.join(directory, weights_name))
+
+    return model
+
+# Set up Command Line Arguments
 parser = argparse.ArgumentParser(description='Apply test examples to trained model.')
-parser.add_argument('--model', help='The datetime for the model', required=True)
+parser.add_argument('-t', '--timestamp', help='The datetime for the model', required=True)
 args = parser.parse_args()
 
-print(args.model)
+#print(args.model)
+
+# Prepare Model
+timestamp = args.timestamp
+
+model = acquire_model(timestamp)
+print(' -- Model loaded from file --')
