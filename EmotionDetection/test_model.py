5236826ddb4dda23ca34a938d0a8c3dee527fa88
7138gmhampton <0407435@abertay.ac.uk>
2020-07-16 15:34:34 +0100

    Compare prediction to truth and log to details file

--- EmotionDetection/test_model.py
+++ EmotionDetection/test_model.py
@@ -1,6 +1,7 @@
 import argparse, os, hyper, numpy
 
 from keras.models import model_from_json, Sequential
+#from keras.layers import
 
 def acquire_model(timestamp):
     directory = hyper.MODEL_DIRECTORY
@@ -13,6 +14,13 @@ def acquire_model(timestamp):
     model.load_weights(os.path.join(directory, weights_name))
 
     return model
+
+def log_accuracy(timestamp, accuracy):
+    details_name = timestamp + '_details.txt'
+
+    with open(os.path.join(hyper.MODEL_DIRECTORY, details_name), 'r+') as details:
+        if 'Test Accuracy: ' not in details.read():
+            details.write('Test Accuracy: ' + '{:1.3f}'.format(accuracy) + '\n')
 
 # Set up Command Line Arguments
 parser = argparse.ArgumentParser(description='Apply test examples to trained model.')
@@ -30,3 +38,25 @@ print(' -- Model loaded from file --')
 # Load Testing Data
 data = numpy.load('ck_test_data.npy')
 labels = numpy.load('ck_test_labels.npy')
+
+# Predict Based on Model and Test Data
+predicted_output = model.predict(data).tolist()
+true_output = labels.tolist()
+
+# Compare Prediction to Truth
+count_of_matches = 0
+prediction_list = []
+true_list = []
+
+for iii in range(len(labels)):
+    predicted_emotion = max(predicted_output[iii])
+    true_emotion = max(true_output[iii])
+    prediction_list.append(predicted_output[iii].index(predicted_emotion))
+    true_list.append(true_output[iii].index(true_emotion))
+    if predicted_output[iii].index(predicted_emotion) == true_output[iii].index(true_emotion):
+        count_of_matches += 1
+
+#print((count_of_matches/len(labels))*100)
+accuracy = (count_of_matches/len(labels))
+print('Accuracy of ' + timestamp +' model: ' + '{:2.2f}'.format(accuracy*100) + '%')
+log_accuracy(timestamp, accuracy)
