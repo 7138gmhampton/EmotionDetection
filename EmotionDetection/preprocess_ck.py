ac1c126b3c91de405389a75fd72b7443c0805132
7138gmhampton <0407435@abertay.ac.uk>
2020-07-22 20:49:36 +0100

    Employ refactorings per linter

--- EmotionDetection/preprocess_ck.py
+++ EmotionDetection/preprocess_ck.py
@@ -28,21 +28,23 @@ from face_extract import excise_face
 TrainableImage = namedtuple('TrainableImage', ['image_array', 'emotion'])
 
 def prepare_image_for_cnn(file, emotion_code, reverse=False):
+    """Load in single image, extract the face and denote the assigned emotion"""
     image = load_img(file, color_mode='grayscale', target_size=None)
     if reverse: image.transpose(Image.FLIP_LEFT_RIGHT)
 
-    image_array = img_to_array(image)
-    face_only = excise_face(image_array, \
+    image_as_array = img_to_array(image)
+    face_only = excise_face(image_as_array, \
         cv2.CascadeClassifier('haarcascade_frontalface_default.xml'))
 
     # return ImageForCNN(face_only, emotion_code)
     return TrainableImage(face_only, emotion_code)
 
-def load_entire_emotion(directory, emotion_code):
+def load_entire_emotion(directory_of_images, emotion_code):
+    """Prepare an entire emotion's worth of images for training"""
     images_of_emotion = []
 
-    for file in os.listdir(os.fsencode(directory)):
-        filename = directory + '\\' + os.fsdecode(file)
+    for file in os.listdir(os.fsencode(directory_of_images)):
+        filename = directory_of_images + '\\' + os.fsdecode(file)
         images_of_emotion.append(prepare_image_for_cnn(filename, emotion_code))
         images_of_emotion.append(prepare_image_for_cnn(filename, emotion_code, True))
 
@@ -66,8 +68,8 @@ for directory in directories:
 random.shuffle(prepared_images)
 
 # Extract Trainable Data and Labels
-dataset = []
-indexed_labels = []
+dataset = [] # pylint: disable=invalid-name
+indexed_labels = [] # pylint: disable=invalid-name
 for prepared_image in prepared_images:
     image_array = numpy.asarray(prepared_image.image_array)
     dataset.append(image_array.astype('float32'))
@@ -78,15 +80,15 @@ print('Dataset Length: ' + str(len(dataset)))
 print('Dataset Entry Shape:' + str(dataset[0].shape))
 print('Dataset Entry Type: ' + str(dataset[0].dtype))
 
-trainable_data = numpy.asarray(dataset)
-trainable_data = numpy.expand_dims(trainable_data, -1)
+trainable_data = numpy.asarray(dataset) # pylint: disable=invalid-name
+trainable_data = numpy.expand_dims(trainable_data, -1) # pylint: disable=invalid-name
 print('Trainable Dataset Shape: ' + str(trainable_data.shape))
 print('Trainable Dataset Type: ' + str(trainable_data.dtype))
 numpy.save('ck_data', trainable_data)
 print(' -- Trainable data saved --')
 
 # Output Labels
-indexed_labels = numpy.asarray(indexed_labels)
+indexed_labels = numpy.asarray(indexed_labels) # pylint: disable=invalid-name
 numpy.save('ck_labels', indexed_labels)
 print(' -- Labels saved -- ')
 
