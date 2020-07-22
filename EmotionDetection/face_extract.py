ddb513b73aaef25f07fef6b6619fb98158deeab2
7138gmhampton <0407435@abertay.ac.uk>
2020-07-22 17:29:58 +0100

    Extract function to excise face from image

--- EmotionDetection/face_extract.py
+++ EmotionDetection/face_extract.py
@@ -3,12 +3,25 @@ import numpy, cv2
 from mtcnn.mtcnn import MTCNN
 from keras.preprocessing.image import load_img, img_to_array
 from matplotlib import pyplot
+from cv2 import CascadeClassifier
 
 print(cv2.__version__)
+
+def excise_face(image_array, classifier):
+    int_array = image_array.astype(numpy.uint8)
+    faces = classifier.detectMultiScale(int_array, 1.3, 5)
+
+    x_start, y_start, width, height = faces[0]
+    x_end, y_end = x_start + width, y_start + height
+    output = int_array[y_start:y_end, x_start:x_end]
+    output = cv2.resize(output, (300,300))
+
+    return output.astype(numpy.float32)
 
 # Load an Image
 raw_image = load_img('example3.png', color_mode='grayscale')
 raw_image = img_to_array(raw_image).squeeze()
+raw_image = raw_image.astype(numpy.uint8)
 raw_image = raw_image.astype(numpy.uint8)
 print(raw_image.shape)
 print(raw_image)
@@ -35,6 +48,34 @@ output_image = cv2.resize(output_image, (300,300))
 print(output_image.shape)
 
 # Display Result
-cv2.imshow('output', output_image)
-cv2.waitKey()
-cv2.destroyAllWindows()
+# output_image = output_image.astype(numpy.float32)
+# cv2.imshow('output', output_image)
+# pyplot.figure()
+# pyplot.imshow(output_image, interpolation='none', cmap='gray')
+# pyplot.show()
+# output_image = output_image.astype(numpy.float32)
+# print(output_image)
+# cv2.waitKey()
+# cv2.destroyAllWindows()
+
+first_image = load_img('example.png', color_mode='grayscale')
+first_image = img_to_array(first_image).squeeze()
+second_image = load_img('example2.png', color_mode='grayscale')
+second_image = img_to_array(second_image).squeeze()
+third_image = load_img('example3.png', color_mode='grayscale')
+third_image = img_to_array(third_image).squeeze()
+
+# first_output = excise_face(first_image, face_cascade)
+# second_output = excise_face(second_image, face_cascade)
+# third_output = excise_face(third_image, face_cascade)
+
+input_images  = [first_image, second_image, third_image]
+
+for full_image in input_images:
+    face_only = excise_face(full_image, face_cascade)
+
+    figure, (before, after) = pyplot.subplots(1,2)
+    before.imshow(full_image, interpolation='none', cmap='gray')
+    after.imshow(face_only, interpolation='none', cmap='gray')
+
+pyplot.show()
