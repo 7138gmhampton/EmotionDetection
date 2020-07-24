fc8da5b7b57ae5a67543fca8d81b61558c1d2a5c
7138gmhampton <0407435@abertay.ac.uk>
2020-07-24 19:11:51 +0100

    Various refactorings per linter

--- EmotionDetection/model_builders.py
+++ EmotionDetection/model_builders.py
@@ -1,51 +1,56 @@
+"""Contains functions to build various models for Facial Expression Recognition"""
 import os
-from hyper import NO_OF_FEATURES, FACE_BOUND_SCALED, DROPOUT, NO_OF_LABELS
 
 os.environ['KERAS_BACKEND'] = 'plaidml.keras.backend'
+# pylint: disable=wrong-import-position
 from keras.models import Sequential
-from keras.layers import Conv2D, BatchNormalization, MaxPooling2D, Dropout, Flatten, Dense
+from keras.layers import Conv2D, BatchNormalization, MaxPooling2D, Dropout,\
+     Flatten, Dense
 from keras.regularizers import l2
 from keras.losses import categorical_crossentropy
 from keras.optimizers import Adam
+from hyper import NO_OF_FEATURES, FACE_BOUND_SCALED, DROPOUT, NO_OF_LABELS
 
 def build_shanks():
+    """Builds model based on one from Nishank Sharma - \
+        github.com/gitshanks/fer2013"""
     model = Sequential()
 
     # First Feature Extraction
     model.add(Conv2D(NO_OF_FEATURES, kernel_size=(3,3), activation='relu',
-        input_shape=(FACE_BOUND_SCALED, FACE_BOUND_SCALED,1),
-        data_format='channels_last', kernel_regularizer=l2()))
-    model.add(Conv2D(NO_OF_FEATURES,kernel_size=(3,3),activation='relu',
-        padding='same'))
+                     input_shape=(FACE_BOUND_SCALED, FACE_BOUND_SCALED, 1),
+                     data_format='channels_last', kernel_regularizer=l2()))
+    model.add(Conv2D(NO_OF_FEATURES, kernel_size=(3, 3), activation='relu',
+                     padding='same'))
     model.add(BatchNormalization())
-    model.add(MaxPooling2D(pool_size=(2,2), strides=(2,2)))
+    model.add(MaxPooling2D(pool_size=(2, 2), strides=(2, 2)))
     model.add(Dropout(DROPOUT))
 
     # Second Feature Extraction
-    model.add(Conv2D(2*NO_OF_FEATURES,kernel_size=(3,3),activation='relu',
-        padding='same'))
+    model.add(Conv2D(2*NO_OF_FEATURES, kernel_size=(3, 3), activation='relu',
+                     padding='same'))
     model.add(BatchNormalization())
-    model.add(Conv2D(2*NO_OF_FEATURES,kernel_size=(3,3),activation='relu',
-        padding='same'))
-    model.add(MaxPooling2D(pool_size=(2,2), strides=(2,2)))
+    model.add(Conv2D(2*NO_OF_FEATURES, kernel_size=(3, 3), activation='relu',
+                     padding='same'))
+    model.add(MaxPooling2D(pool_size=(2, 2), strides=(2, 2)))
     model.add(Dropout(DROPOUT))
 
     # Third Feature Extraction
-    model.add(Conv2D(2*2*NO_OF_FEATURES,kernel_size=(3,3),activation='relu',
-        padding='same'))
+    model.add(Conv2D(2*2*NO_OF_FEATURES, kernel_size=(3, 3), activation='relu',
+                     padding='same'))
     model.add(BatchNormalization())
-    model.add(Conv2D(2*2*NO_OF_FEATURES,kernel_size=(3,3),activation='relu', 
-        padding='same'))
-    model.add(MaxPooling2D(pool_size=(2,2), strides=(2,2)))
+    model.add(Conv2D(2*2*NO_OF_FEATURES ,kernel_size=(3, 3), activation='relu',
+                     padding='same'))
+    model.add(MaxPooling2D(pool_size=(2, 2), strides=(2, 2)))
     model.add(Dropout(DROPOUT))
 
     # Fourth Feature Extraction
-    model.add(Conv2D(2*2*2*NO_OF_FEATURES,kernel_size=(3,3),activation='relu', 
-        padding='same'))
+    model.add(Conv2D(2*2*2*NO_OF_FEATURES, kernel_size=(3, 3), activation='relu',
+                     padding='same'))
     model.add(BatchNormalization())
-    model.add(Conv2D(2*2*2*NO_OF_FEATURES,kernel_size=(3,3),activation='relu', 
-        padding='same'))
-    model.add(MaxPooling2D(pool_size=(2,2), strides=(2,2)))
+    model.add(Conv2D(2*2*2*NO_OF_FEATURES, kernel_size=(3, 3), activation='relu',
+                     padding='same'))
+    model.add(MaxPooling2D(pool_size=(2, 2), strides=(2, 2)))
     model.add(Dropout(DROPOUT))
 
     # Classifier
@@ -62,6 +67,6 @@ def build_shanks():
 
     # Compile Model
     model.compile(loss=categorical_crossentropy, optimizer=Adam(), 
-        metrics=['accuracy'])
+                  metrics=['accuracy'])
 
     return model
