7b5f648821dc905c1d186719ce65cf7a7385b4a2
7138gmhampton <0407435@abertay.ac.uk>
2020-07-29 18:03:11 +0100

    Various refactors per linter

--- EmotionDetection/progress_bar.py
+++ EmotionDetection/progress_bar.py
@@ -6,21 +6,19 @@ def progress_bar(iterable, decimals=1, length=100, fill='█', print_end='\r', *
     :param suffix: Text to appear after bar
     """
     total = len(iterable)
-    # if 'suffix' in kwargs: suffix = kwargs['suffix']
-    # else: suffix = ''
     suffix = kwargs['suffix'] if 'suffix' in kwargs else ''
     prefix = kwargs['prefix'] if 'prefix' in kwargs else ''
-    
+
     def print_progress_bar(iteration):
         percent = ('{0:.' + str(decimals) + 'f}').format(100*(iteration/float(total)))
         filled_length = int(length*iteration//total)
-        bar = fill * filled_length + '-' * (length - filled_length)
-        print(f'\r{prefix} |{bar}| {percent}% {suffix}', end = print_end)
-        
+        display_bar = fill * filled_length + '-' * (length - filled_length)
+        print(f'\r{prefix} |{display_bar}| {percent}% {suffix}', end=print_end)
+
     print_progress_bar(0)
-    
+
     for iii, item in enumerate(iterable):
         yield item
         print_progress_bar(iii + 1)
-        
+
     print()
