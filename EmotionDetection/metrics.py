16ffcfe3f053754ed1636c909cc594b73fd1c3f8
7138gmhampton <0407435@abertay.ac.uk>
2020-08-03 18:26:34 +0100

    Remove corpse code

--- EmotionDetection/metrics.py
+++ EmotionDetection/metrics.py
@@ -12,17 +12,13 @@ def author_confusion_matrix(true, predicted, timestamp):
     matrix_filename = timestamp + '_confusion.png'
     matrix = confusion_matrix(true, predicted, normalize='true')
     
-    # figure = pyplot.figure()
     figure, axis = pyplot.subplots()
     matrix_image = axis.imshow(matrix, interpolation='nearest', cmap=pyplot.cm.get_cmap(name='Blues'))
-    # axis.title('Confusion Matrix')
     axis.set(title='Confusion Matrix')
     figure.colorbar(matrix_image)
     tick_marks = numpy.arange(len(labels))
-    # axis.xticks(tick_marks, labels, rotation=45)
     axis.set_xticks(tick_marks)
     axis.set_xticklabels(labels)
-    # axis.yticks(tick_marks, labels)
     axis.set_xticks(tick_marks)
     axis.set_xticklabels(labels)
     threshold = matrix.max()/2
@@ -30,9 +26,7 @@ def author_confusion_matrix(true, predicted, timestamp):
     for iii, jjj in product(range(matrix.shape[0]), range(matrix.shape[1])):
         axis.text(jjj, iii, format(matrix[iii, jjj], '0.2f'), horizontalalignment='center', color='white' if matrix[iii,jjj] > threshold else 'black')
         
-    # axis.ylabel('True Label')
     axis.set_ylabel('True Label')
-    # axis.xlabel('Predicted Label')
     axis.set_xlabel('Predicted Label')
     figure.tight_layout()
     
