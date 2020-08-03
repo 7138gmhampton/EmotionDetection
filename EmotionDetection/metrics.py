9712973f860695d329bbeb24279b510b66ed0ece
7138gmhampton <0407435@abertay.ac.uk>
2020-08-03 18:13:53 +0100

    Extract function for logging details for trained model

--- EmotionDetection/metrics.py
+++ EmotionDetection/metrics.py
@@ -4,7 +4,7 @@ import itertools
 from itertools import product
 import matplotlib.pyplot as pyplot
 from sklearn.metrics import confusion_matrix
-from hyper import MODEL_DIRECTORY
+from hyper import MODEL_DIRECTORY, SCALE_DOWN_FACTOR, BATCH_SIZE
 
 labels = ['joy', 'disgust', 'anger', 'fear', 'sadness', 'surprise', 'neutral']
 
@@ -37,3 +37,16 @@ def author_confusion_matrix(true, predicted, timestamp):
     figure.tight_layout()
     
     figure.savefig(os.path.join(MODEL_DIRECTORY, matrix_filename))
+    
+def log_details(timestamp, history, epochs, model):
+    filename = timestamp + '_details.txt'
+    
+    with open(os.path.join(MODEL_DIRECTORY, filename), 'w') as text_file:
+        text_file.write('Model Used: ' + model + '\n')
+        text_file.write('Training Accuracy: ' + '{:.3f}'.format(history['acc'][-1]) + '\n')
+        text_file.write('Validation Accuracy: ' + '{:.3f}'.format(history['val_acc'][-1]) + '\n')
+        text_file.write('Note the above metrics may not represent the checkpointed model\n')
+        text_file.write('Scale Down Factor: ' + str(SCALE_DOWN_FACTOR) + '\n')
+        text_file.write('Batch Size: ' + str(BATCH_SIZE) + '\n')
+        text_file.write('No. of Epochs: ' + str(len(history['acc'])) + '/' + str(epochs) + '\n')
+        
